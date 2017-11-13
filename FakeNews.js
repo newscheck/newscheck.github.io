945676af517764ae7a6a7b11045032dd7fef6f8f
Susie <susiebriley@gmail.com>
2017-11-13 08:35:11 -0600

    fake news

--- FakeNews.js
+++ FakeNews.js
@@ -1,5 +1,20 @@
+$(document).ready(function() {
+    $("#snopes").html('<object data="https://www.snopes.com/playing-christmas-music-federal-crime/"/>');
+    if($('.rating').hasClass('false')) {
+        alert("false");
+    }
+})
+
+$('#submit').click(function() {
+    urlProcess();
+})
+
 function urlProcess() {
   var x=document.getElementById("url");
   var text = x.elements[0].value;
   document.getElementById("dis_url").innerHTML = text;
+}
+
+function getResult() {
+    $('.claim').hide();
 }

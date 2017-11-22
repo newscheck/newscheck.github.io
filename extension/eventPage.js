1745cfbf4807130d48a8f90f1748726c38abd2a9
chunankong <chenxu2018@u.northwestern.edu>
2017-11-22 12:21:43 -0600

    changed to highlight search

---
+++ extension/eventPage.js
@@ -0,0 +1,26 @@
+var menuItem = {
+    "id": "Snopesit",
+    "title": "Snopesit",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI (str) {
+    return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Snopesit" && clickData.selectionText){
+        var snopesURL = "https://www.snopes.com/?s=" + fixedEncodeURI(clickData.selectionText);
+        var createData = {
+            "url": snopesURL,
+            "type": "popup",
+            "top": 5,
+            "left": 5,
+            "width": screen.availWidth,
+            "height": screen.availHeight
+        };
+        chrome.windows.create(createData, function(){});
+    }
+});
